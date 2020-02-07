5d6d9afd5f6df70641165987a2251503b00383e4
co-ban <60699121+co-ban@users.noreply.github.com>
2020-02-07 21:35:09 +0900

    Update priconne_AttackManagement.py

--- priconne_AttackManagement.py
+++ priconne_AttackManagement.py
@@ -16,6 +16,8 @@ async def on_message(message):
     # ".凸管理@数字"で起動
     if message.content.startswith(".凸管理"):
 
+    await message.channel.send(message.channel.id)
+
         # チャンネル制御
         if message.channel.id == channelid:
 
